883cbe8fb1535d64b5c494cce6ca37344af6580c
Miks29 <mikerenzomanosca@gmail.com>
2022-03-25 14:02:55 +0800

    update7
    hide password when typing

--- PlanTech/app/src/main/java/com/plantech/myapplication/SignUpActivity.java
+++ PlanTech/app/src/main/java/com/plantech/myapplication/SignUpActivity.java
@@ -16,7 +16,10 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.SignInMethodQueryResult;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -26,7 +29,7 @@ public class SignUpActivity extends AppCompatActivity {
     ProgressDialog progressDialog;
     TextView alreadyHaveAnAccount;
 
-    FirebaseAuth mAuth;
+    FirebaseAuth myAuth;
     FirebaseUser mUser;
 
 
@@ -42,8 +45,8 @@ public class SignUpActivity extends AppCompatActivity {
         alreadyHaveAnAccount = findViewById(R.id.alreadyHaveAnAccount);
         buttonSignUp = findViewById(R.id.buttonSignUp);
         progressDialog = new ProgressDialog(this);
-        mAuth = FirebaseAuth.getInstance();
-        mUser = mAuth.getCurrentUser();
+        myAuth = FirebaseAuth.getInstance();
+        mUser = myAuth.getCurrentUser();
 
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +63,20 @@ public class SignUpActivity extends AppCompatActivity {
         });
 
     }
+
+    /*public void checkEmailifAlreadyUsed(){
+        myAuth.fetchSignInMethodsForEmail(inputEmail.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
+                       Boolean new_email = !task.getResult().getSignInMethods().isEmpty();
+
+                       if (new_email){
+                           inputEmail.setError("Email already taken");
+                       }
+                    }
+                });
+    }*/
 
     private void PerformAuth(){
         String username = inputUsername1.getText().toString();
@@ -86,7 +103,7 @@ public class SignUpActivity extends AppCompatActivity {
             progressDialog.setCanceledOnTouchOutside(false);
             progressDialog.show();
 
-            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            myAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()){
@@ -97,7 +114,84 @@ public class SignUpActivity extends AppCompatActivity {
                     else
                     {
                         progressDialog.dismiss();
-                        Toast.makeText(SignUpActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                        String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
+
+                        switch (errorCode) {
+
+                            case "ERROR_INVALID_CUSTOM_TOKEN":
+                                Toast.makeText(SignUpActivity.this, "The custom token format is incorrect. Please check the documentation.", Toast.LENGTH_LONG).show();
+                                break;
+
+                            case "ERROR_CUSTOM_TOKEN_MISMATCH":
+                                Toast.makeText(SignUpActivity.this, "The custom token corresponds to a different audience.", Toast.LENGTH_LONG).show();
+                                break;
+
+                            case "ERROR_INVALID_CREDENTIAL":
+                                Toast.makeText(SignUpActivity.this, "The supplied auth credential is malformed or has expired.", Toast.LENGTH_LONG).show();
+                                break;
+
+                            case "ERROR_INVALID_EMAIL":
+                                //Toast.makeText(SignUpActivity.this, "The email address is badly formatted.", Toast.LENGTH_LONG).show();
+                                inputEmail.setError("The email address is badly formatted.");
+                                inputEmail.requestFocus();
+                                break;
+
+                            case "ERROR_WRONG_PASSWORD":
+                                //Toast.makeText(SignUpActivity.this, "The password is invalid or the user does not have a password.", Toast.LENGTH_LONG).show();
+                                inputPass.setError("password is incorrect ");
+                                inputPass.requestFocus();
+                                inputPass.setText("");
+                                break;
+
+                            case "ERROR_USER_MISMATCH":
+                                Toast.makeText(SignUpActivity.this, "The supplied credentials do not correspond to the previously signed in user.", Toast.LENGTH_LONG).show();
+                                break;
+
+                            case "ERROR_REQUIRES_RECENT_LOGIN":
+                                Toast.makeText(SignUpActivity.this, "This operation is sensitive and requires recent authentication. Log in again before retrying this request.", Toast.LENGTH_LONG).show();
+                                break;
+
+                            case "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL":
+                                Toast.makeText(SignUpActivity.this, "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.", Toast.LENGTH_LONG).show();
+                                break;
+
+                            case "ERROR_EMAIL_ALREADY_IN_USE":
+                                //Toast.makeText(SignUpActivity.this, "The email address is already in use by another account.   ", Toast.LENGTH_LONG).show();
+                                inputEmail.setError("The email address is already in use by another account.");
+                                inputEmail.requestFocus();
+                                break;
+
+                            case "ERROR_CREDENTIAL_ALREADY_IN_USE":
+                                Toast.makeText(SignUpActivity.this, "This email is already associated with a different user account.", Toast.LENGTH_LONG).show();
+                                break;
+
+                            case "ERROR_USER_DISABLED":
+                                Toast.makeText(SignUpActivity.this, "The user account has been disabled by an administrator.", Toast.LENGTH_LONG).show();
+                                break;
+
+                            case "ERROR_USER_TOKEN_EXPIRED":
+                                Toast.makeText(SignUpActivity.this, "The user\\'s credential is no longer valid. The user must sign in again.", Toast.LENGTH_LONG).show();
+                                break;
+
+                            case "ERROR_USER_NOT_FOUND":
+                                Toast.makeText(SignUpActivity.this, "There is no user record corresponding to this identifier. The user may have been deleted.", Toast.LENGTH_LONG).show();
+                                break;
+
+                            case "ERROR_INVALID_USER_TOKEN":
+                                Toast.makeText(SignUpActivity.this, "The user\\'s credential is no longer valid. The user must sign in again.", Toast.LENGTH_LONG).show();
+                                break;
+
+                            case "ERROR_OPERATION_NOT_ALLOWED":
+                                Toast.makeText(SignUpActivity.this, "This operation is not allowed. You must enable this service in the console.", Toast.LENGTH_LONG).show();
+                                break;
+
+                            case "ERROR_WEAK_PASSWORD":
+                                //Toast.makeText(SignUpActivity.this, "The given password is invalid.", Toast.LENGTH_LONG).show();
+                                inputPass.setError("The password is invalid it must 8 characters at least");
+                                inputPass.requestFocus();
+                                break;
+
+                        }
                     }
                 }
             });
@@ -106,8 +200,10 @@ public class SignUpActivity extends AppCompatActivity {
 
     private void openLoginActivity() {
         Intent login_act = new Intent(SignUpActivity.this,LogInActivity.class);
-        login_act.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        //login_act.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(login_act);
 
     }
+
+
 }
